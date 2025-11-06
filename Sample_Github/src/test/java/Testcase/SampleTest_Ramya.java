e38faa44ec574bb03cf280eb66a1af810176a0eb
Sandeepramya <sandeepramya01062023@outlook.com>
2025-11-06 15:06:02 +0530

    SampleTest commit

---
+++ Sample_Github/src/test/java/Testcase/SampleTest_Ramya.java
@@ -0,0 +1,13 @@
+package Testcase;
+
+public class SampleTest_Ramya {
+	
+	public static void main(String[] args) {
+		System.out.println("create contact");
+		System.out.println("create contact with org");
+		System.out.println("create contact with phonenumber");
+		System.out.println("create org");
+		System.out.println("create org with phone number");
+	}
+
+}
